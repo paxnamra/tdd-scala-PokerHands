0b1023a53bf4d04ffa818c8ec4519d06467a1d99
Paxnamra <bialyfeniks4@gmail.com>
2020-04-12 04:53:23 +0200

    poker hands || sortBy clean up and refactor

--- src/main/scala/PokerHand.scala
+++ src/main/scala/PokerHand.scala
@@ -4,9 +4,13 @@ object PokerHand {
     .toMap
 
   case class Card(figure: Char, power: Int)
+  case class CardOccurrence(occurrence: Int, cardPower: Int, figure: Char)
+
+  implicit val cardOccurrenceOrdering: Ordering[CardOccurrence] =
+  Ordering.by[CardOccurrence, (Int, Int)](co => (co.occurrence, co.cardPower)).reverse
 
   def evaluate(hand: String): String = {
-    val value: List[(Int, Int, Char)] = hand
+    val cardOccurrences: List[CardOccurrence] = hand
       .split(" ")
       .map { card =>
         card.toList match {
@@ -15,17 +19,17 @@ object PokerHand {
       }
       .groupBy(_.figure)
       .map {
-        case (figure, cards) => (cards.size, cards.head.power, figure)
+        case (figure, cards) => CardOccurrence(cards.size, cards.head.power, figure)
       }
       .toList
-      .sorted(Ordering[(Int, Int, Char)].reverse)
-      .sortBy(_._2)(Ordering[Int].reverse)
-    
-    val (occurrences, _, figure) = value.head
-    if (occurrences == 2) {
-      "pair of : " + figure
+      .sorted
+
+
+    val maxCardOccurrence = cardOccurrences.head
+    if (maxCardOccurrence.occurrence == 2) {
+      "pair of : " + maxCardOccurrence.figure
     } else {
-      "high card : " + figure
+      "high card : " + maxCardOccurrence.figure
     }
   }
 }
